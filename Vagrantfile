3160c126cdfc54cd1ad640def2e4ad992053001c
Anes Lozo <aneslozo@gmail.com>
2016-04-21 16:47:47 +0200

    Configuring nginx and adding demo site

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,7 @@
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.hostname = "aerial-dev"
+  config.vm.network "private_network", ip: "192.168.1.10"
 
   config.vm.provision "shell", path: "provision.sh"
-  config.vm.network "forwarded_port", guest: 80, host: 8080, id: "nginx"
 end
--- provision.sh
+++ provision.sh
@@ -1,5 +1,19 @@
+#!/usr/bin/env bash
+
 apt-get -y update
 
+# nginx
 apt-get -y install nginx
-
 service nginx start
+
+# set up nginx server
+cp /vagrant/nginx/default /etc/nginx/sites-available/default
+chmod 644 /etc/nginx/sites-available/default
+ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+service nginx restart
+
+# clean /var/www
+rm -rf /var/www
+
+# symlink /var/www => /vagrant/www
+ln -s /vagrant/www /var/www
